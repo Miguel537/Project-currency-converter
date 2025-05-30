7bc6f46d5e8c11c04c2a5c4904c546fe70ed0acf
Miguel Joaquim <migueljoaquim409@gmail.com>
2025-05-30 20:03:24 -0300

    Adicionadno a opção de const

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ document.getElementById('currency-form').addEventListener
 ('submit', function(event){
     event.preventDefault();
 
-    
-})
+    //Obter valores de entrada
+    const valor = document.getElementById('amount').value;
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+});
