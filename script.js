a17fcd69eb898ef9fefe681d689bd791d467c7e4
Miguel Joaquim <migueljoaquim409@gmail.com>
2025-05-30 20:10:47 -0300

    Adicionando as opções de conversão para que ela funcione perfeitamente

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ document.getElementById('currency-form').addEventListener
     const valor = document.getElementById('amount').value;
     const daMoeda = document.getElementById('daMoeda').value;
     const paraMoeda = document.getElementById('paraMoeda').value;
+
+    //Definir taxas de câmbio
+    const exchangeRates = {
+        USD: { BRL: 5.73, EUR: 0.88 },
+        BRL: { USD: 0.17, EUR: 0.15 },
+        EUR: { USD: 1.13, BRL: 6.50 }
+    };
 });
